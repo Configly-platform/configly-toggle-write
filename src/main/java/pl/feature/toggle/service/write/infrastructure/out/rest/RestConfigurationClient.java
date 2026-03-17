a50311d19676cacc1b0b68e4f12a8b8d9a3fc9bb
SaQie <Kamcio99@interia.com>
2026-03-17 19:11:04 +0100

    Rest configuration client implementation

--- src/main/java/pl/feature/toggle/service/write/infrastructure/out/rest/RestConfigurationClient.java
+++ src/main/java/pl/feature/toggle/service/write/infrastructure/out/rest/RestConfigurationClient.java
@@ -1,22 +1,75 @@
 package pl.feature.toggle.service.write.infrastructure.out.rest;
 
 import lombok.AllArgsConstructor;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.RestClientException;
 import pl.feature.toggle.service.model.environment.EnvironmentId;
 import pl.feature.toggle.service.model.project.ProjectId;
+import pl.feature.toggle.service.model.security.correlation.CorrelationId;
+import pl.feature.toggle.service.model.security.correlation.CorrelationProvider;
 import pl.feature.toggle.service.write.application.port.out.ConfigurationClient;
 import pl.feature.toggle.service.write.domain.reference.EnvironmentRef;
 import pl.feature.toggle.service.write.domain.reference.ProjectRef;
+import pl.feature.toggle.service.write.infrastructure.out.rest.dto.EnvironmentRefDto;
+import pl.feature.toggle.service.write.infrastructure.out.rest.dto.ProjectRefDto;
+import pl.feature.toggle.service.write.infrastructure.out.rest.exception.ConfigurationServiceResponseException;
+
+import java.util.Map;
 
 @AllArgsConstructor
 class RestConfigurationClient implements ConfigurationClient {
 
+    private final RestClient restClient;
+    private final CorrelationProvider correlationProvider;
+
     @Override
     public ProjectRef fetchProject(ProjectId projectId) {
-        return null;
+        var uri = "/internal/projects/{projectId}/reference";
+        return fetch(uri, ProjectRefDto.class, projectId.idAsString())
+                .toReference();
     }
 
     @Override
     public EnvironmentRef fetchEnvironment(ProjectId projectId, EnvironmentId environmentId) {
-        return null;
+        var uri = "/internal/projects/{projectId}/environments/{environmentId}/reference";
+        return fetch(uri, EnvironmentRefDto.class, projectId.idAsString(), environmentId.idAsString())
+                .toReference();
     }
+
+    private <T> T fetch(String uri, Class<T> responseClass, Object... parameters) {
+        try {
+            var result = restClient.get()
+                    .uri(uri, parameters)
+                    .header(CorrelationId.headerName(), correlationProvider.current().value())
+                    .retrieve()
+                    .body(responseClass);
+            if (result == null) {
+                throw new ConfigurationServiceResponseException(
+                        "Empty response from configuration service",
+                        context(uri, null)
+                );
+            }
+            return result;
+        } catch (RestClientException ex) {
+            throw new ConfigurationServiceResponseException(
+                    "Failed to fetch reference from configuration service",
+                    context(uri, ex)
+            );
+        }
+    }
+
+    private Map<String, Object> context(String uri, Exception ex) {
+        var contextBuilder = ContextBuilder.create()
+                .with("uri", uri)
+                .with("correlationId", correlationProvider.current());
+        if (ex != null) {
+            contextBuilder.with("exception", ex);
+            if (ex instanceof HttpClientErrorException httpEx) {
+                contextBuilder.with("statusCode", httpEx.getStatusCode());
+            }
+        }
+        return contextBuilder.build();
+    }
+
 }
--- src/main/java/pl/feature/toggle/service/write/infrastructure/out/rest/RestOutboundConfig.java
+++ src/main/java/pl/feature/toggle/service/write/infrastructure/out/rest/RestOutboundConfig.java
@@ -1,14 +1,24 @@
 package pl.feature.toggle.service.write.infrastructure.out.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
+import pl.feature.toggle.service.model.security.correlation.CorrelationProvider;
 import pl.feature.toggle.service.write.application.port.out.ConfigurationClient;
 
 @Configuration
 class RestOutboundConfig {
 
     @Bean
-    ConfigurationClient configurationClient() {
-        return new RestConfigurationClient();
+    ConfigurationClient configurationClient(RestClient restClient, CorrelationProvider correlationProvider) {
+        return new RestConfigurationClient(restClient, correlationProvider);
+    }
+
+    @Bean
+    RestClient configurationRestClient(@Value("${client.configuration.base-url}") String baseUrl) {
+        return RestClient.builder()
+                .baseUrl(baseUrl)
+                .build();
     }
 }
